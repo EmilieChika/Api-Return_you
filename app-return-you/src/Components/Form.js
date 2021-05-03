bb0c78892d0428b6fd20d9359c82ca6953c509e9
broutaaxel <you@example.com>
2021-05-03 03:20:29 +0200

    Creation of crud file for recover the data

--- app-return-you/src/Components/Form.js
+++ app-return-you/src/Components/Form.js
@@ -5,19 +5,19 @@ function Form() {
     return (
         <div >
             <form >
-                <label htmlFor = 'firstname' > Prénom < /label>
+                <label htmlFor = 'firstname' > Prénom </label>
                     <input type = 'text' id = 'firstname' firstname = 'firstname' placeholder="Entrer votre prénom"/>
 
-                    <label htmlFor = 'lastname' > Nom < /label>
+                    <label htmlFor = 'lastname' > Nom </label>
                         <input type = 'text' id = 'lastname' lastname = 'lastname' placeholder="Entrer votre nom"/>
 
-                        <label htmlFor = 'email' > Email < /label>
+                        <label htmlFor = 'email' > Email </label>
                             <input type = 'text' id = 'email' email = 'email'placeholder="Entrer votre email" />
 
-                            <label htmlFor = 'appointment' > Titre rendez-vous < /label>
+                            <label htmlFor = 'appointment' > Titre rendez-vous </label>
                                 <input type = 'text' id = 'appointment' appointment = 'appointment'placeholder="Entrez l'intitulé" />
 
-                                     <label htmlFor = 'comments' > Commentaires < /label>
+                                     <label htmlFor = 'comments' > Commentaires </label>
                                          <input type = 'text' id = 'comments'comments = 'comments'placeholder="Ajouter un commentaire" />
 
                             <button > Envoyer le rendez - vous </button>
@@ -27,6 +27,7 @@ function Form() {
         </div>
 
 )
+
 }
 
 export default Form
